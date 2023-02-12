1c49221df4dbbd6c802a9097ef0335a7167340e9
Ahmed <ahmedhassan88616@gmail.com>
2023-02-12 11:12:45 +0200

    second commit

--- lib/home.dart
+++ lib/home.dart
@@ -1,32 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   final _key = UniqueKey();
+
   late WebViewController _webViewController;
+  bool isBtnEnabled = false;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: WillPopScope(
+    return WillPopScope(
       onWillPop: () async {
-        if (await _webViewController.canGoBack()) {
-          await _webViewController.goBack();
-          return false;
-        }
-        return true;
+        return await _willExit();
       },
       child: SafeArea(
-        child: WebView(
-          key: _key,
-          javascriptMode: JavascriptMode.unrestricted,
-          initialUrl: 'http://54.244.198.217/DemoEzzar/',
-          gestureNavigationEnabled: true,
-          zoomEnabled: true,
-          onWebViewCreated: (WebViewController controller) {
-            _webViewController = controller;
-          },
-        ),
+        child: Scaffold(
+            appBar: AppBar(
+              leading: Builder(builder: (childContext) {
+                return IconButton(
+                  onPressed: isBtnEnabled
+                      ? () async {
+                          await _willExit();
+                        }
+                      : null,
+                  icon: Icon(
+                    Icons.arrow_back_ios,
+                    color: isBtnEnabled ? Colors.white : Colors.white70,
+                    size: 26.0,
+                  ),
+                );
+              }),
+              backgroundColor: HexColor('#236361'), /*Colors.red,*/
+            ),
+            body: Stack(
+              children: [
+                WebView(
+                  key: _key,
+                  javascriptMode: JavascriptMode.unrestricted,
+                  initialUrl:
+                      'https://www.google.com/', //'http://54.244.198.217/DemoEzzar/',
+                  gestureNavigationEnabled: true,
+                  zoomEnabled: true,
+                  onWebViewCreated: (WebViewController controller) {
+                    _webViewController = controller;
+                  },
+                  onPageFinished: (str) async {
+                    isBtnEnabled = await _webViewController.canGoBack();
+                    setState(() {});
+                  },
+                ),
+              ],
+            )),
       ),
-    ));
+    );
+  }
+
+  Future<bool> _willExit() async {
+    if (await _webViewController.canGoBack()) {
+      await _webViewController.goBack();
+      return false;
+    }
+    return true;
   }
 }
